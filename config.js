f3637d3a7bcc9de5e191cfce4ce40211441cd3a3
officialanjanahe <officialanjanaheroku@gmail.com>
2025-07-13 18:00:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'Manu-MD&L1oESQDT#vsJh8YOGg510_4Vpimyw-fncs9gEgbxgj2VFAQo14VQ', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
+SESSION_ID: process.env.SESSION_ID || 'Manu-MD&30BRzKwA#ZrMJF84dfb56U23rYDcpS6re_9X6urAd4NhI6b_n2A4', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000',
 MONGODB: process.env.MONGODB || 'mongodb+srv://lelumanjana1:IrzKBkH1OXFr6Vhs@cluster0.5bizj.mongodb.net/' // අනිවාරෙන් Mongodb Url එකක් ඕනි දන්නෙ නැත්නම් මගෙ වීඩියෝ එක බලලා හදාගන්න.🔥
 };
